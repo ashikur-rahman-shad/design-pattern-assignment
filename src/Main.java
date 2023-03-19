075f1de73618ed5dd7b1a4216e478d740daabb10
Ashikur Rahman Shad <shadgood@gmail.com>
2023-03-19 12:45:31 +0600

    public static ArrayList of seller is created It will contain all the sellers and their stuffs

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
+import java.util.*;
 public class Main {
+    public static ArrayList<Seller> seller = new ArrayList<Seller>();
     public static void main(String[] args) {
-        System.out.println("Hello me");
     }
+
 }
-//IntelliJ Idea Project of my Design Pattern assignment: https://elearn.daffodilvarsity.edu.bd/mod/assign/view.php?id=1285314
