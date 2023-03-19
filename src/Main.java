bc94a8f6c9d07bc4644f60e83aa0f3e61e08d0f7
Ashikur Rahman Shad <shadgood@gmail.com>
2023-03-19 13:17:23 +0600

    Seller class tested by adding a new seller and products

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-import java.util.*;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
+        Seller.addSeller("Walid","Demra","88017711728191");
+        Seller.list.get(0).addProduct("Potato",12.5);
+        Seller.list.get(0).addProduct("Rice", 14.5);
+        Seller.list.get(0).showProducts();
     }
-
 }
