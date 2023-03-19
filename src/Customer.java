678269c889ac2d8ec7ca90047fb232177bf7f54e
Ashikur Rahman Shad <shadgood@gmail.com>
2023-03-19 16:43:51 +0600

    Encapsulation fixed

---
+++ src/Customer.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class Customer {
+    private static ArrayList<Customer> list = new ArrayList<Customer>();
+
+    private Cart cart = new Cart();
+    private String name;
+    private String email;
+    private String address;
+    private String contact_no;
+
+    private Customer(String name, String email, String address, String contact_no) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.contact_no = contact_no;
+    }
+
+    public static Customer no(int id) {
+        return list.get(id);
+    }
+
+    public static void add(String name, String email, String address, String contact_no) {
+        list.add(new Customer(name, email, address, contact_no));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact_no() {
+        return contact_no;
+    }
+
+    public void setContact_no(String contact_no) {
+        this.contact_no = contact_no;
+    }
+
+    public Cart cart() {
+        return this.cart;
+    }
+
+}
