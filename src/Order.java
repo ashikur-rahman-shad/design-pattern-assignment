b62e880fb8d859bbabb39cc10e94547cf7b26060
Ashikur Rahman Shad <shadgood@gmail.com>
2023-03-19 16:40:20 +0600

    Order class created

---
+++ src/Order.java
@@ -0,0 +1,25 @@
+public class Order {
+    private Product product;
+    private int quantity;
+
+    Order(Product product, int quantity) {
+        this.setProduct(product);
+        this.setQuantity(quantity);
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
