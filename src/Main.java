d2f188b3c3258b84d438f86e89691631d40b45e5
Ashikur Rahman Shad <shadgood@gmail.com>
2023-03-19 17:31:06 +0600

    OnlineBanks tested

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
+import OnlineBank.Bkash;
+import OnlineBank.Nagad;
+import OnlineBank.Rocket;
+
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+
+        Bkash bkash = new Bkash();
+        Nagad nagad = new Nagad();
+        Rocket rocket = new Rocket();
+
         Seller.add("Walid", "Demra", "88017711728191");
+
         Seller.no(0).addProduct("Potato", 12.5);
         Seller.no(0).addProduct("Rice", 14.5);
         Seller.no(0).showProducts();
+
 
         Customer.add("Mohiuddin", "mohuddin69@hotmail.com", "Charabag", "012344569");
 
@@ -13,5 +24,11 @@ public class Main {
         Customer.no(0).cart().add(Seller.no(0).selectProduct(0), 4);
         Customer.no(0).cart().add(Seller.no(0).selectProduct(1), 1);
         Customer.no(0).cart().show();
+
+        System.out.println("\nGrand Total: BDT " + Customer.no(0).cart().grandTotal());
+        System.out.println("\nOn Bkash: \tBDT "+ bkash.calculateCharge(Customer.no(0).cart().grandTotal()));
+        System.out.println("On Nagad: \tBDT "+ nagad.calculateCharge(Customer.no(0).cart().grandTotal()));
+        System.out.println("On Bkash: \tBDT "+ rocket.calculateCharge(Customer.no(0).cart().grandTotal()));
+
     }
 }
