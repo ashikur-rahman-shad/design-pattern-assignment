5f0b163b01d419c8637d8f854259e891efab40c1
Ashikur Rahman Shad <shadgood@gmail.com>
2023-03-19 17:16:47 +0600

    A default method to calculate charge is added to the OnlineBank.java interface

--- src/OnlineBank/OnlineBank.java
+++ src/OnlineBank/OnlineBank.java
@@ -2,5 +2,10 @@ package OnlineBank;
 
 public interface OnlineBank {
     String getName();
+
     double getCharge();
+
+    default double calculateCharge(double amount) {
+        return amount + ((amount / 100) * this.getCharge());
+    }
 }
