efe6fc63a260a6b12055dc8428a1cb121506b28c
Ashikur Rahman Shad <shadgood@gmail.com>
2023-03-19 12:36:40 +0600

    Creating the project

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello me");
+    }
+}
+//IntelliJ Idea Project of my Design Pattern assignment: https://elearn.daffodilvarsity.edu.bd/mod/assign/view.php?id=1285314
